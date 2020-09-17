05599f77d1ed1fcfbcf7c3a99a2a71d106c7b4de
bhavani-goruganthu <bgoruganthu@mail.sfsu.edu>
2020-09-16 17:05:40 -0700

    Committed build folder

---
+++ build/precache-manifest.6b8ccf1f823464ce395318e5c644d431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af1663a7e300f259364dabfb90c9fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a12511b4108b4c564ce",
+    "url": "/static/css/main.c24e91df.chunk.css"
+  },
+  {
+    "revision": "694e2bb2f56fb404b5ee",
+    "url": "/static/js/2.3020fa6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3020fa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a12511b4108b4c564ce",
+    "url": "/static/js/main.8b58f28f.chunk.js"
+  },
+  {
+    "revision": "1079aa53ffb29e09491a",
+    "url": "/static/js/runtime-main.71adf852.js"
+  },
+  {
+    "revision": "578fd3acbc0692e1dcea287fde66f652",
+    "url": "/static/media/students.578fd3ac.jpg"
+  }
+]);
